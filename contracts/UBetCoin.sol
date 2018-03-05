c7dddfaad6778d686ffc96aef4522a2ab9d2a88e
Paul Lopez <plopez@iprecheck.com>
2018-03-05 13:06:40 +0800

    update SYMBOL

--- contracts/UBetCoin.sol
+++ contracts/UBetCoin.sol
@@ -5,7 +5,7 @@ contract UBetCoin is StandardToken {
   string public name = "UbetCoin";
   string public symbol = "UBET";
   uint public decimals = 3;
-  uint public INITIAL_SUPPLY = 10000 * (10 ** decimals);
+  uint public INITIAL_SUPPLY = 2400000000 * (10 ** decimals);
 
   function UBetCoin() public {
     totalSupply_ = INITIAL_SUPPLY;
